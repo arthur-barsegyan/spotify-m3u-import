0c3e9536d94663224de01c632773f6b68d405b32
Arthur Barsegyan <a.barsegyan96@gmail.com>
2018-08-25 01:13:17 +0300

    Implement search tracks from iTunes playlist

--- spotify-playlist-import.py
+++ spotify-playlist-import.py
@@ -28,26 +28,29 @@ def parse_arguments():
 def load_playlist_file(playlist_file):
     tracks = []
 
-    def parse_m3u():
+    def _parse_m3u():
         content = [line.strip() for line in playlist_file if line.strip() and not line.startswith("#")]
 
         for track in content:
             tracks.append({'path': track})
 
-    def parse_xml():
-    	# 'readPlist' is deprecated but new method available only in Python 3
+    def _parse_xml():
+        # 'readPlist' is deprecated but new method available only in Python 3
         playlist = plistlib.readPlist(playlist_file)
         for track_meta in playlist['Tracks'].values():
             artist = track_meta['Artist'] if 'Artist' in track_meta else ''
             name = track_meta['Name'] if 'Name' in track_meta else ''
-            tracks.append({'path': artist + ' - ' + name})
+            tracks.append({'artist': artist, 'name': name, 'source': 'itunes'})
 
     path_with_name, file_ext = os.path.splitext(playlist_file.name)
+    read_id3_tags = False
     try:
         if file_ext == '.m3u':
-            parse_m3u()
+            _parse_m3u()
+            read_id3_tags = True
         elif file_ext == '.xml':
-            parse_xml()
+            _parse_xml()
+            # If we have iTunes XML we don't need read ID3 tags because we have all information
         else:
             print colored('Playlist file failed load: Unsupported file extension "%s"', 'red')
             sys.exit(1)
@@ -56,11 +59,11 @@ def load_playlist_file(playlist_file):
         logger.critical('Playlist file "%s" failed load: %s' % (playlist_file.name, str(e)))
         sys.exit(1)
     else:
-        return tracks
+        return {'tracks': tracks, 'read_id3': read_id3_tags}
 
 
 def read_id3_tags(file_name):
-    tag_data = False
+    tag_data = None
     try:
         track_id3 = eyed3.load(file_name)
     except Exception as e:
@@ -69,19 +72,18 @@ def read_id3_tags(file_name):
         logger.debug('Reading tags from "%s"' % file_name)
         if track_id3.tag is not None:
             if track_id3.tag.artist is not None and track_id3.tag.title is not None:
-                tag_data = {'artist': track_id3.tag.artist, 'title': track_id3.tag.title}
+                tag_data = {'artist': track_id3.tag.artist, 'name': track_id3.tag.title}
     return tag_data
 
 
 def guess_missing_track_info(file_name):
-    guess = False
+    guess = None
     filename = os.path.basename(file_name)
     filename_no_ext = os.path.splitext(filename)[0]
     track_uri_parts = filename_no_ext.split('-')
     if len(track_uri_parts) > 1:
-        guess = {'filename': {},
-                 'artist': track_uri_parts[0].strip(),
-                 'title': track_uri_parts[1].strip()}
+        guess = {'artist': track_uri_parts[0].strip(),
+                 'name': track_uri_parts[1].strip()}
     return guess
 
 
@@ -99,58 +101,50 @@ def find_spotify_track(sp, track):
             for spotify_result in spotify_results:
                 spotify_result['rank'] = _how_similar(track_name, spotify_result['name'])
                 if spotify_result['rank'] == 1.0:
-                    return {'id': spotify_result['id'], 'title': spotify_result['name'],
+                    return {'id': spotify_result['id'], 'name': spotify_result['name'],
                             'artist': spotify_result['artists'][0]['name']}
             spotify_results_sorted = sorted(spotify_results, key=lambda k: k['rank'], reverse=True)
             if len(spotify_results_sorted) > 0 and spotify_results_sorted[0]['rank'] > spotify_match_threshold:
-                return {'id': spotify_results_sorted[0]['id'], 'title': spotify_results_sorted[0]['name'],
+                return {'id': spotify_results_sorted[0]['id'], 'name': spotify_results_sorted[0]['name'],
                         'artist': spotify_results_sorted[0]['artists'][0]['name']}
         logger.debug('No good Spotify result found')
         return False
 
     spotify_match_threshold = 0.5
-    # search by id3 tags
-    if track['id3_data'] and 'artist' in track['id3_data'] and 'title' in track['id3_data']:
-        spotify_search_string = '%s %s' % (track['id3_data']['artist'], track['id3_data']['title'])
+ 
+    if 'artist' in track and 'name' in track:
+        spotify_search_string = '%s %s' % (track['artist'], track['name'])
         search_result = _select_result_from_spotify_search(
             spotify_search_string,
-            track['id3_data']['title'],
+            track['name'],
             spotify_match_threshold
         )
+
         if search_result:
             return search_result
-    # search by track['guess']
-    if 'guess' in track and track['guess'] and 'artist' in track['guess'] and 'title' in track['guess']:
-        spotify_search_string = '%s %s' % (track['guess']['artist'], track['guess']['title'])
-        search_result = _select_result_from_spotify_search(
-            spotify_search_string,
-            track['guess']['title'],
-            spotify_match_threshold
-        )
-        if search_result:
-            return search_result
+    
     return False
 
 
 def format_track_info(track):
-    if track['id3_data']:
-        formatted_id3_data = '%s - %s' % (repr(track['id3_data']['artist']), repr(track['id3_data']['title']))
+    if track['source'] == 'id3':
+        formatted_id3_data = '%s - %s' % (repr(track['artist']), repr(track['name']))
         formatted_guess = 'Not required'
     else:
         formatted_id3_data = colored('None', 'red')
-        if track['guess']:
-            formatted_guess = '%s - %s' % (repr(track['guess']['artist']), repr(track['guess']['title']))
+        if track['source'] == 'guess':
+            formatted_guess = '%s - %s' % (repr(track['artist']), repr(track['name']))
         else:
             formatted_guess = colored('None', 'red')
     if track['spotify_data']:
         formatted_spotify = colored('%s - %s, %s' % (
                                     repr(track['spotify_data']['artist']),
-                                    repr(track['spotify_data']['title']), repr(track['spotify_data']['id'])),
+                                    repr(track['spotify_data']['name']), repr(track['spotify_data']['id'])),
                                     'green')
     else:
         formatted_spotify = colored('None', 'red')
     return '\n%s\nIDv3 tag data: %s\nGuess from filename: %s\nSpotify: %s' % (
-        colored(repr(track['path']), 'blue'),
+        colored(repr(track['path']), 'blue') if track['source'] != 'itunes' else colored(repr(track['artist'] + ' - ' + track['name']), 'blue'),
         formatted_id3_data,
         formatted_guess,
         formatted_spotify
@@ -186,16 +180,23 @@ def main():
         eyed3.log.setLevel("ERROR")
         stdout_level = logging.CRITICAL
 
-    tracks = load_playlist_file(args.file)
+    data = load_playlist_file(args.file)
+    tracks = data['tracks']
+    try_read_tags = data['read_id3']
 
     print colored('Parsed %s tracks from %s' % (len(tracks), args.file.name), 'green')
 
     for track in tracks:
-        track['id3_data'] = read_id3_tags(track['path'])
-        if not track['id3_data']:
-            track['guess'] = guess_missing_track_info(track['path'])
+        if try_read_tags is True:
+            id3_data = read_id3_tags(track['path'])
+            if id3_data is not None:
+                track.update(id3_data, source='id3')
+            else:
+                guess_data = guess_missing_track_info(track['path'])
+                if guess_data is not None:
+                    track.update(guess_data, source='guess')
+            
         track['spotify_data'] = find_spotify_track(sp, track)
-
         print format_track_info(track)
 
     spotify_tracks = [k['spotify_data']['id'] for k in tracks if k.get('spotify_data')]
